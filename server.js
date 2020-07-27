76f718719f96b166ff78f717d62744f792825aa7
Dwight Huggins <dwighthuggins@DwightHugginsJr.local>
2020-07-27 17:20:12 -0400

    middlware added to server.js

--- server.js
+++ server.js
@@ -3,6 +3,9 @@ const app = express();
 
 const PORT = process.env.PORT || 8080;
 
+app.use(express.urlencoded({ extended: true}));
+app.use(express.json());
+
 app.listen(PORT, () => {
-    console.log(`Server is running on http://localhost:${PORT}`);
+    console.log(`Server is running on http://localhost:${PORT}`)
 });
